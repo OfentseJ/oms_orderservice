62d3592bc79697b5d1e776eadfda83380a4f4b05
Ofentse <ofentsemakhutja@icloud.com>
2026-09-21 09:29:57 +0200

    done 18.09.2026

--- src/main/java/com/fnb/orderservice/security/impl/JwtAuthFilter.java
+++ src/main/java/com/fnb/orderservice/security/impl/JwtAuthFilter.java
@@ -1,4 +1,34 @@
 package com.fnb.orderservice.security.impl;
 
-public class JwtAuthFilter {
+import com.fnb.orderservice.security.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private static final String HEADER_AUTHORIZATION = "Authorization";
+
+    private static final String BEARER_PREFIX = "Bearer ";
+
+    private final JwtService jwtService;
+
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+    }
 }
